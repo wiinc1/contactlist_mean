76767adc55089b854261ade178b3b5fc967b7613
Brian Wuollet <bwuollet@is-mbp-bwuollet3.in.ionicsecurity.com>
2017-07-28 14:20:51 -0400

    adding logic for new contact, delete contact

--- routes/route.js
+++ routes/route.js
@@ -1,19 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
+const Contact = require('../models/contacts');
+
 //retrieving contacts
 router.get('/contacts',(req, res, next)=>{
-    res.send('Rerieving the contact list');
+    Contact.find(function(err, contacts){
+        res.json(contacts);
+    })
 });
 
 //add contact
 router.post('/contact',(req, res, next)=>{
-    //logic to add contact
+    let newContact = new Contact({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone: req.body.phone
+    });
+    
+    newContact.save((err, contact)=>{
+        if(err)
+        {
+            res.json({msg: 'Failed to add contact'});
+        }    
+        else{
+            res.json({msg: 'Contact added succesfully'});;
+        }
+    })
 });
 
 //deleting contact
 router.delete('/contact/:id',(req, res, next)=>{
-    //logic to add contact
+    Contact.remove({_id: req.params.id}, function(err, result){
+        if(err)
+        {
+            res.json(err);
+        }
+        else{
+            res.json(result);
+        }
+    });
 });
 
 
