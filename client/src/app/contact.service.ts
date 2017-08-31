de27cb21d55703189bf4c42bf8865a3891363940
Brian Wuollet <bwuollet@is-mbp-bwuollet3.in.ionicsecurity.com>
2017-08-31 14:22:42 -0400

    comitt

--- client/src/app/contact.service.ts
+++ client/src/app/contact.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import {Http, Header} from '@angular/http';
+import {Contact} from './contact';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class ContactService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  
 
 }
